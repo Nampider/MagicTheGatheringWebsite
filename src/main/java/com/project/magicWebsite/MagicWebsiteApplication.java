2ded36b7ec2f578d5d67804f3a660d008d92b97e
Nampider <devchrisnam@gmail.com>
2026-01-25 13:12:01 -0800

    first commit for website

---
+++ src/main/java/com/project/magicWebsite/MagicWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.project.magicWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/magicWebsite/MagicWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.magicWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
